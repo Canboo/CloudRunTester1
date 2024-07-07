b3d70d02bf5d2900cc84144c81c4c7bd01d1ef24
Tsai Cheng Peng <service@canboo.tw>
2024-07-07 23:08:06 +0800

    feat: 存取 GCS

--- Controllers/GCPController.cs
+++ Controllers/GCPController.cs
@@ -3,6 +3,7 @@ using Google.Cloud.SecretManager.V1;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+using Google.Cloud.Storage.V1;
 
 namespace api.Controllers;
 
@@ -10,14 +11,18 @@ namespace api.Controllers;
 [Route("[controller]")]
 public class GCPController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
     private readonly ApiContext db;
-    public GCPController(ApiContext db)
+    public GCPController(ApiContext db, IConfiguration configuration)
     {
         _client = SecretManagerServiceClient.Create();
+        _storageClient = StorageClient.Create();
         this.db = db;
+        _configuration = configuration;
     }
 
     private readonly SecretManagerServiceClient _client;
+    private readonly StorageClient _storageClient;
 
     /// <summary>嘗試取出 Secret Manager 的設定值</summary>
     /// <param name="projectId">請輸入專案Id</param>
@@ -49,5 +54,31 @@ public class GCPController : ControllerBase
     public async Task<ActionResult<IEnumerable<TaiwanCity>>> GetSQL()
     {
         return await db.TaiwanCities.ToListAsync();
+    }
+
+    /// <summary>嘗試取出 GCS 檔案</summary>
+    /// <param name="filename">輸入要取得的檔案名稱</param>
+    /// <response code="204">新增成功</response>
+    [HttpGet("/GCS/{filename}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetGCS(string filename)
+    {
+        string projectId = _configuration["GCP:ProjectId"];
+        try
+        {
+            SecretVersionName secretVersionName = new(projectId, "gcs-name", "latest");
+            AccessSecretVersionResponse result = await _client.AccessSecretVersionAsync(secretVersionName);
+            string secretValue = result.Payload.Data.ToStringUtf8();
+
+            var memoryStream = new MemoryStream();
+            await _storageClient.DownloadObjectAsync(secretValue, filename, memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return File(memoryStream, "application/octet-stream", filename);
+        }
+        catch (Google.GoogleApiException e)
+        {
+            return NotFound(e.Message);
+        }
     }
 }
